398294505b29e15fd56f9d4e3f6d6241bebb889d
Adrian Frühwirth <bonki@users.noreply.github.com>
2018-07-04 16:39:08 +0200

    SAGA: Fix parameter name inconsistency in Font::textDraw()

--- engines/saga/font.h
+++ engines/saga/font.h
@@ -142,8 +142,8 @@ class Font {
 	int getHeight(KnownFont font, const char *text, int width, FontEffectFlags flags) {
 		return getHeight(knownFont2FontIdx(font), text, width, flags);
 	}
-	void textDraw(KnownFont font, const char *string, const Common::Point &point, int color, int effectColor, FontEffectFlags flags) {
-		textDraw(knownFont2FontIdx(font), string, point, color, effectColor, flags);
+	void textDraw(KnownFont font, const char *text, const Common::Point &point, int color, int effectColor, FontEffectFlags flags) {
+		textDraw(knownFont2FontIdx(font), text, point, color, effectColor, flags);
 	}
 	void textDrawRect(KnownFont font, const char *text, const Common::Rect &rect, int color, int effectColor, FontEffectFlags flags) {
 		textDrawRect(knownFont2FontIdx(font), text, rect, color, effectColor, flags);
@@ -169,7 +169,7 @@ class Font {
 	 int getStringWidth(FontId fontId, const char *text, size_t count, FontEffectFlags flags);
 	 int getHeight(FontId fontId, const char *text, int width, FontEffectFlags flags);
 	 void textDrawRect(FontId fontId, const char *text, const Common::Rect &rect, int color, int effectColor, FontEffectFlags flags);
-	 void textDraw(FontId fontId, const char *string, const Common::Point &point, int color, int effectColor, FontEffectFlags flags);
+	 void textDraw(FontId fontId, const char *text, const Common::Point &point, int color, int effectColor, FontEffectFlags flags);
 
 	 void loadFont(FontData *font, uint32 fontResourceId);
 	 void createOutline(FontData *font);
